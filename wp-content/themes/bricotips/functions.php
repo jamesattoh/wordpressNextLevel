36b5ef23e341b0cbad79e1fc1400f97d17faeddf
jamesattoh <koss.attoh27@gmail.com>
2025-03-30 20:36:19 +0100

    footer customizing with widget

--- wp-content/themes/bricotips/functions.php
+++ wp-content/themes/bricotips/functions.php
@@ -2,9 +2,28 @@
 
 // Action qui permet de charger des scripts dans notre thème
 add_action('wp_enqueue_scripts', 'theme_enqueue_styles');
-function theme_enqueue_styles(){
+function theme_enqueue_styles()
+{
     // Chargement du style.css du thème parent Twenty Twenty
     wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
     // Chargement du css/theme.css pour nos personnalisations
     wp_enqueue_style('theme-style', get_stylesheet_directory_uri() . '/css/theme.css', array(), filemtime(get_stylesheet_directory() . '/css/theme.css')); //le 4eme paramètre (filemtime) permet de gérer la version du fichier, et donc d’éviter tout problème de cache
+    // Chargement du /css/widgets/image-titre-widget.css pour notre widget image titre
+    wp_enqueue_style('image-titre-widget', get_stylesheet_directory_uri() . '/css/widgets/image-titre-widget.css', array(), filemtime(get_stylesheet_directory() . '/css/widgets/image-titre-widget.css'));
 }
+
+
+/* CHARGEMENT DES WIDGETS */
+
+
+// On créer une class Widget Image_Titre_Widget dans un fichier à part pour pas surcharger le functions.php
+require_once(__DIR__ . '/widgets/ImageTitreWidget.php');
+
+
+function register_widgets()
+{
+    //On enregistre le widget avec la class Image_Titre_Widget
+    register_widget('Image_Titre_Widget');
+}
+//On demander à wordpress de charger des widget selon la fonction register_widgets()
+add_action('widgets_init', 'register_widgets');
