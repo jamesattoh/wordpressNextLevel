d2b0bae808e564e6611b81c2ba0c0224e2c7d854
jamesattoh <koss.attoh27@gmail.com>
2025-04-02 02:04:33 +0100

    template separation into sub-sections

---
+++ wp-content/themes/bricotips/template-parts/bloc-lexique.php
@@ -0,0 +1,12 @@
+<div class="mot">
+    <div class="titre">
+        <?= $args['titre'] ?> <!-- àconsulter dans la documentation https://developer.wordpress.org/reference/functions/get_template_part/ -->
+        <div class="action">
+            <div class="down">&#9660;</div>
+            <div class="up">&#9650;</div>
+        </div>
+    </div>
+    <div class="definition">
+        <?= $args['description'] ?>
+    </div>
+</div>
